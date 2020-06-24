c4e61a111a88df1d50f6b146fb01c5941dbe8b5d
AlexTicovschi <alexmticovschi@gmail.com>
2020-06-24 15:13:06 +0100

    fetching jobs posted by company

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -18,6 +18,36 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-md-12">
+            <table class="table">
+                <thead>
+                    <th></th>
+                    <th>Position</th>
+                    <th>Address</th>
+                    <th>Date</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach($company->jobs as $job)
+                        <tr>
+                            <td><img src="{{asset('avatar/man.jpg')}}" width="80" alt=""></td>
+                            <td>{{$job->position}}
+                                <br>
+                                <i class="fa fa-clock-o"></i> {{$job->type}}
+                            </td>
+                            <td><i class="fa fa-map-marker"></i> {{$job->address}}</td>
+                            <td><i class="fa fa-globe"></i><br>{{$job->last_date}}</td>
+                            <td>
+                                <a href="/jobs/{{$job->id}}/{{$job->slug}}">
+                                    <button class="btn btn-success btn-sm">Apply</button>
+                                </a>
+                            </td>
+                        </tr> 
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @endsection
