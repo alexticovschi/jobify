427af5062d391b3b5bb50a17d4d19c982f988822
AlexTicovschi <alexmticovschi@gmail.com>
2020-06-30 12:10:58 +0100

    adding cdn links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,5 +80,9 @@
             @yield('content')
         </main>
     </div>
+    <script  src="https://code.jquery.com/jquery-1.12.4.js"></script>
+
+    <script defer src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
