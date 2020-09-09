299108eeee3c9d33cf1a4071d7d48941612c0bd7
Alex Ticovschi <alexmticovschi@gmail.com>
2020-09-09 13:25:07 +0100

    login page redesign

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -101,16 +101,23 @@
                                                 </li>
                                                 <li>
                                                     <!-- Button trigger modal -->
-                                                    <a href="#">
-                                                        <span
-                                                            data-toggle="modal"
+  
+                                                    @if(!Auth::check())
+                                                        <button data-toggle="modal"
                                                             data-target="#exampleModal"
-                                                            className="bg-primary text-white py-3 px-4 rounded"
-                                                        >
-                                                            <span className="icon-user mr-3"></span>
-                                                            Login
-                                                        </span>
-                                                    </a>
+                                                            class="btn btn-primary text-white py-2 px-3 rounded"><span class="icon-user mr-3"></span>
+                                                            Login</button>
+                                                    @else 
+                                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault();
+                                                                        document.getElementById('logout-form').submit();">
+                                                            {{ __('Logout') }}
+                                                        </a>
+
+                                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                            @csrf
+                                                        </form>
+                                                    @endif
 
                                                 </li>
                                             </ul>
@@ -136,12 +143,68 @@
         </button>
       </div>
       <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+        <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-12 col-form-label text-md-left">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-12 col-form-label text-md-left">{{ __('Password') }}</label>
+
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-12">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+        </form>
+      
     </div>
   </div>
 </div>
