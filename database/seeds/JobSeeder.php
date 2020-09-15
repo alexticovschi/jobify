f00615402ba4b7d4242599a00a67a5940abfb894
Alex Ticovschi <alexmticovschi@gmail.com>
2020-09-15 10:29:24 +0100

    making migration for jobs

--- database/seeds/JobSeeder.php
+++ database/seeds/JobSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
-use App\User;
 use App\Company;
+use App\User;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
 
 class JobSeeder extends Seeder
 {
@@ -16,20 +16,24 @@ class JobSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        foreach(range(1,100) as $index){
+        foreach (range(1, 100) as $index) {
             DB::table('jobs')->insert([
                 'user_id' => User::all()->random()->id,
                 'company_id' => Company::all()->random()->id,
-                'title' => $title=$faker->text,
+                'title' => $title = $faker->text,
                 'slug' => Str::slug($title),
-                'description' => $faker->paragraph(rand(2,10)),
-                'roles'=> $faker->text,
+                'description' => $faker->paragraph(rand(2, 10)),
+                'roles' => $faker->text,
                 'address' => $faker->address,
-                'category_id' => rand(1,5),
+                'category_id' => rand(1, 5),
                 'position' => $faker->jobTitle,
                 'type' => 'fulltime',
                 'status' => rand(0, 1),
                 'last_date' => $faker->DateTime,
+                'number_of_vacancy' => rand(1, 10),
+                'experience' => rand(1, 10),
+                'gender' => $faker->randomElement(['any', 'male', 'female']),
+                'salary' => rand(10000, 70000),
             ]);
         }
     }
